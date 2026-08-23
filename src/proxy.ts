8ec5f3ddcf50957a40126be8547cae3be5c0937e
AlbaCasas <albacasasarzua1b@gmail.com>
2026-08-23 10:59:19 +0100

    feat(proxy): add proxy function to redirect requests to the athletes list

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   return NextResponse.redirect(new URL("/athletes", request.url));
 }
 
